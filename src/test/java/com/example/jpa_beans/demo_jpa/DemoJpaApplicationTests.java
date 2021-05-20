f88761016ae9b784f1e925fb6ea8724c9da27e88
singhtaran1005 <singhh3010@gmail.com>
2021-05-20 18:32:51 +0530

    Initial commit

---
+++ src/test/java/com/example/jpa_beans/demo_jpa/DemoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_beans.demo_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
