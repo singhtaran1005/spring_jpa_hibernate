d5882a5a6d62a954e956db06e1a4fa405d58f3b9
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-05-28 18:28:28 +0530

    sample_bean

--- src/main/java/com/example/jpa_beans/demo_jpa/DemoJpaApplication.java
+++ src/main/java/com/example/jpa_beans/demo_jpa/DemoJpaApplication.java
@@ -2,12 +2,23 @@ package com.example.jpa_beans.demo_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.lang.annotation.Annotation;
 
 @SpringBootApplication
 public class DemoJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJpaApplication.class, args);
 
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+//        ApplicationContext
+        ctx.register(MyConfig.class);
+        ctx.refresh();  //to prevent illegal state exception
+        Hello obj =  ctx.getBean(Hello.class);
+        System.out.println(obj);
+    }
 }
