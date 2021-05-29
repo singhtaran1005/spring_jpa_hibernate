334640d6c77b438c9f7326163908113118a74326
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-05-29 23:47:34 +0530

    Update DemoJpaApplication.java

--- src/main/java/com/example/jpa_beans/demo_jpa/DemoJpaApplication.java
+++ src/main/java/com/example/jpa_beans/demo_jpa/DemoJpaApplication.java
@@ -14,11 +14,19 @@ public class DemoJpaApplication {
         SpringApplication.run(DemoJpaApplication.class, args);
 
 
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+//        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 //        ApplicationContext
-        ctx.register(MyConfig.class);
-        ctx.refresh();  //to prevent illegal state exception
-        Hello obj =  ctx.getBean(Hello.class);
-        System.out.println(obj);
+//        ctx.register(MyConfig.class);
+//        ctx.refresh();  //to prevent illegal state exception
+//        Hello obj = ctx.getBean(Hello.class);
+//        Hello obj2 = ctx.getBean(Hello.class);
+//
+//        obj.setData("12");
+//        obj.setMessage("Hello everyone!");
+//        System.out.println(obj);
+//        System.out.println(obj2);
     }
 }
+
+
+//whenever we run appln some other context is running !
